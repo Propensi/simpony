69d583756d6e315405b21929e49cbda11f1e4e4b
ayusaida <ayuzulfa27@gmail.com>
2016-05-04 21:45:45 +0700

    Edit View-Show
    Nyesuai-in kolom yang ditampilin

--- resources/views/programs/show.blade.php
+++ resources/views/programs/show.blade.php
@@ -7,12 +7,12 @@
         <table class="table table-bordered table-striped table-hover">
             <thead>
                 <tr>
-                    <th>ID.</th> <th>Prog ID</th><th>Prog Nama</th><th>Jadwal Tayang</th>
+                    <th>Prog ID</th><th>Nama Program</th><th>Deskripsi</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>{{ $program->id }}</td> <td> {{ $program->Prog_ID }} </td><td> {{ $program->Prog_Nama }} </td><td> {{ $program->Jadwal_Tayang }} </td>
+                    <td> {{ $program->Prog_ID }} </td><td> {{ $program->Prog_Nama }} </td><td> {{ $program->Prog_Deskripsi }} </td>
                 </tr>
             </tbody>    
         </table>
