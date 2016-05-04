510b21397a140a78457d72358668f24d8b3dace9
ayusaida <ayuzulfa27@gmail.com>
2016-05-04 22:04:20 +0700

    Add view summary-index
    Buat penyesuaian kolom tampilan index summary

---
+++ resources/views/summary/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h1>Summary <a href="{{ url('summary/create') }}" class="btn btn-primary pull-right btn-sm">Add New Summary</a></h1>
+    <div class="table">
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>No</th><th>Prog Nama</th><th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            {{-- */$x=0;/* --}}
+            @foreach($summary as $item)
+                {{-- */$x++;/* --}}
+                <tr>
+                    <td>{{ $x }}</td>
+                    <td><a href="{{ url('summary', $item->Sum_ID) }}">{{ $item->Prog_Nama }}</a></td>
+                    <td>
+                        <a href="{{ url('summary/' . $item->Sum_ID . '/edit') }}">
+                            <button type="submit" class="btn btn-primary btn-xs">Update</button>
+                        </a> /
+                        {!! Form::open([
+                            'method'=>'DELETE',
+                            'url' => ['summary', $item->Sum_ID],
+                            'style' => 'display:inline'
+                        ]) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <div class="pagination"> {!! $summary->render() !!} </div>
+    </div>
+
+@endsection
