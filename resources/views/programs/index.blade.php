4b16aa894188ff6cd88e960c314a513a43031799
ayusaida <ayuzulfa27@gmail.com>
2016-05-12 21:07:45 +0700

    Ganti Confirmation Delete
    "Apakah Anda Yakin Untuk Menghapus Data Ini?"

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -38,7 +38,7 @@
 
     <script>
     $(".delete").on("submit", function(){
-        return confirm("Do you want to delete this item?");
+        return confirm("Apakah Anda Yakin Untuk Menghapus Data Ini?");
     });
 </script>
 @endsection
