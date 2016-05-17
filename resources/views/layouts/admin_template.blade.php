a3adbe834ab364e272cdc739a61c6938410d131a
adhikadhanes <adhika.dhaneswara@gmail.com>
2016-05-17 16:37:04 +0700

    nambah master plugin

--- resources/views/layouts/admin_template.blade.php
+++ resources/views/layouts/admin_template.blade.php
@@ -53,6 +53,10 @@
           <!-- bootstrap wysihtml5 - text editor -->
           <link rel="stylesheet" href="{{ asset("/bower_components/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css")}}">
 
+          <link type="text/css" href="{{ asset("/bower_components/adminlte/plugins/timepicker/bootstrap-timepicker.min.css" )}}"/>
+
+          <script type="text/javascript" src="{{ asset("/bower_components/adminlte/plugins/timepicker/bootstrap-timepicker.min.js" )}}"></script>
+
           <script src"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/7.0.2/bootstrap-slider.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/7.0.2/bootstrap-slider.min.js"></script>
           <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/7.0.2/css/bootstrap-slider.css" rel="stylesheet">
@@ -90,6 +94,7 @@
     <!-- REQUIRED JS SCRIPTS -->
 
     <!-- jQuery 2.1.3 -->
+    <script type="text/javascript" src="{{ asset("/bower_components/adminlte/plugins/timepicker/bootstrap-timepicker.min.js" )}}"></script>
     <script src="{{ asset ("/bower_components/adminlte/plugins/jQuery/jQuery-2.2.0.min.js") }}"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="{{ asset ("/bower_components/adminlte/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
