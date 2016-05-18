1dd3371b24cf55c7ecf766bc914f436134a84fde
adhikadhanes <adhika.dhaneswara@gmail.com>
2016-05-18 16:15:55 +0700

    pake status

--- resources/views/research/pelacakan.blade.php
+++ resources/views/research/pelacakan.blade.php
@@ -6,7 +6,7 @@
         <table class="table table-bordered table-striped table-hover">
             <thead>
                 <tr>
-                    <th>No</th><th>Nama Program</th><th>Tanggal</th><th>Pengirim</th><th>Deskripsi</th><th>Actions</th>
+                    <th>No</th><th>Nama Program</th><th>Tanggal</th><th>Pengirim</th><th>Status</th><th>Deskripsi</th><th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,21 +18,32 @@
                     <td><a href="{{ url('assignments2', $item->Assn_ID) }}">{{ $item->program->Prog_Nama }}</a></td>
                     <td>{{ $item->Tanggal }}</td>
                     <td>{{ $item->user->name }}</td>
+                    <td>{{ $item->Status }}</td>
                     <td>{{ $item->Deskripsi }}</td>
                     <td>
-                        <a href="{{ url('assignments2/' . $item->Assn_ID . '/staff') }}">
-                            <button type="submit" class="btn btn-primary btn-xs">Membuat Summary</button>
-                        </a> /  
+
+                            {!! Form::model($assignments2, [
+                                              'method' => 'PATCH',
+                                              'url' => ['assignments2/update', $item->Assn_ID],
+                                              'style'=>'display:inline-block' ]) !!}
+                                              
+                                               {!! Form::hidden('Status', 'Proses' ) !!}
+
+                                     {!! Form::submit('Terima', ['class' => 'btn btn-primary btn-xs '])  !!}
+                                      
+                                      {!! Form::close() !!}
+                         /
                                     {!! Form::model($assignments2, [
                                               'method' => 'PATCH',
                                               'url' => ['assignments2/update', $item->Assn_ID],
-                                              'class' => '']) !!}
+                                              'style'=>'display:inline-block' ]) !!}
                                               
                                                {!! Form::hidden('Status', 'Ditolak' ) !!}
 
                                      {!! Form::submit('Tolak', ['class' => 'btn btn-danger btn-xs'])  !!}
                                       
-                                      {!! Form::close() !!}      
+                                      {!! Form::close() !!}   
+
                                 </td>
                 </tr>
             @endforeach
